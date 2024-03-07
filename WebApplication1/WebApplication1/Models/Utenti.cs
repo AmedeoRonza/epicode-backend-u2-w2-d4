90d8b424d91974afd86f2d1f62a839d0a2028462
Amedeo Ronza <amedeoronza@gmail.com>
2024-03-07 16:26:02 +0100

    CommitFinale

--- WebApplication1/WebApplication1/Models/Utenti.cs
+++ WebApplication1/WebApplication1/Models/Utenti.cs
@@ -9,7 +9,10 @@ namespace WebApplication1.Models
     public class Utenti
     {
         public int IdUtente { get; set; }
+        [Required]
+        [Display(Name = "Nome Utente")]
         public string NomeUtente { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         public string Password {  get; set; }
         public string Ruolo {  get; set; }
